2d66e1d5ad91600ed8b0b6b251923967bcca8325
yunseoashley <helloits-ashley@naver.com>
2024-11-19 19:49:33 +0900

    number game update

--- numbergame2.py
+++ numbergame2.py
@@ -3,12 +3,20 @@ import random
 number = random.randint(1, 100)
 print("i'm thinking of a number between 1 and 100")
 
-while True:
-    guess = int(input("enter your guess: "))
+attempts = 5
+
+while attempts > 0:
+    guess = int(input(f"enter your guess ({attempts} attempts left): "))
+    
     if guess < number:
         print("too low!")
     elif guess > number:
         print("too high!")
     else:
         print("congratulations!")
-        break
+        break  
+    
+    attempts -= 1  
+
+if attempts == 0:
+    print(f"sorry, the number was >{number}<")
