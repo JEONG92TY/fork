2b1465c56e83be94c068e2663f42e7bca2cbd306
yunseoashley <helloits-ashley@naver.com>
2024-11-19 19:43:56 +0900

    number game

--- numbergame.py
+++ numbergame.py
@@ -6,9 +6,9 @@ print("i'm thinking of a number between 1 and 100")
 while True:
     guess = int(input("enter your guess: "))
     if guess < number:
-        print("too low!")
+        print("too low")
     elif guess > number:
-        print("too high!")
+        print("too high")
     else:
         print("congratulations!")
         break
